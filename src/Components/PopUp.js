f7d35a9ab6e7e69f78efe3d221dbacac37a40776
Juned <jund7734@gmail.com>
2023-04-05 06:02:01 +0530

    bug solved

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './PopUp.css'
 function PopUp({cls,visible}) {
   return (
-    <div className={`border-2 ${visible} glow border-white text-white p-2 rounded-md fixed top-16 right-5`} >
+    <div className={`border-2 z-50 ${visible} glow border-white text-white p-2 rounded-md fixed top-16 right-5`} >
         `{cls}` copied to your clipboard.
     </div>
   )
