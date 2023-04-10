1c81b5ebf4a8ce62330427f6e1e1ae4629880fdf
Juned <jund7734@gmail.com>
2023-04-10 17:45:10 +0530

    linkdin link added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import './footer.css';
 function Footer() {
   return (
     <div className='bg-gray-300 p-12 mt-16 '>
-        <h1 className='text-center text-xl ubuntuMono'>Developed and Maintained by <a href='https://github.com/iamjunedkhan' rel="noopener noreferrer" target='_blank' className='text-blue-700 underline hover:no-underline'>Juned Khan</a></h1>
+        <h1 className='text-center text-xl ubuntuMono'>Developed and Maintained by <a href='https://www.linkedin.com/in/iamjunedkhan/' rel="noopener noreferrer" target='_blank' className='text-blue-700 underline hover:no-underline'>Juned Khan</a></h1>
     </div>
 
   )
